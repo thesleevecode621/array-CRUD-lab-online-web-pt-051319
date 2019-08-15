d0deb4ceb9102b252f6421761e8c971d2fd5fc17
boruch d <dubywider@gmail.com>
2019-08-15 17:05:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array.push("nala cat")
+  array.push("element")
 end
 
 def add_element_to_start_of_array(array, element)
